64e730af11c968d3427c5afab0cb39ccad8b21a3
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-12-08 20:50:19 +0700

    update card view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Img from "./components/Img";
 import humberger from "./assets/humberger.png";
+import love from "./assets/love.png";
 
 function App() {
   return (
     <div className="bg-black pb-5">
       {/* Nav */}
-      <div className="flex justify-between bg-black text-white space-x-2 p-2 py-3 border-b-2 border-gray-300">
+      <div className="flex justify-between bg-black text-white space-x-2 p-2 py-3 border-b-2 border-black">
         <img className="h-10 w-10 p-1" src={humberger} alt="image" />
         <input
           className="w-5/12 rounded-md px-3 bg-gray-300 bg-opacity-25"
@@ -29,10 +30,10 @@ function App() {
           possimus.
         </p>
         <div className="flex space-x-2">
-          <button className="p-1 px-2 text-white text-sm bg-gray-600 rounded-sm">
+          <button className="p-1 px-2 text-white text-sm bg-gray-600 rounded-lg">
             Start Create
           </button>
-          <button className="p-1 px-2 text-white text-sm bg-gray-600 rounded-sm">
+          <button className="p-1 px-2 text-white text-sm bg-gray-600 rounded-lg">
             How it Work
           </button>
         </div>
@@ -42,80 +43,69 @@ function App() {
       <div className="flex flex-col space-y-2 m-2">
         <h1 className="text-lg text-white font-bold">Explore</h1>
         <ul className="flex flex-nowrap justify-start space-x-2 overflow-x-auto">
-          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-md">
+          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-full border-2 border-white">
             List items
           </li>
-          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-md">
+          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-full">
             List items
           </li>
-          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-md">
+          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-full">
             List items
           </li>
-          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-md">
+          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-full">
             List items
           </li>
-          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-md">
+          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-full">
             List items
           </li>
-          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-md">
+          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-full">
             List items
           </li>
-          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-md">
+          <li className="flex-shrink-0 p-2 bg-gray-300 bg-opacity-20 text-white rounded-full">
             List items
           </li>
         </ul>
       </div>
 
       {/* Card List */}
-      <div className="flex space-x-3 m-2 flex-nowrap overflow-x-auto">
+      <div className="flex space-x-3 mt-4 m-2 flex-nowrap overflow-x-auto">
         <div className="flex-shrink-0 p-3 w-72 h-80 rounded-lg bg-gray-300 bg-opacity-30 backdrop-filter blur-xl">
           <div className="flex flex-col space-y-3">
-            <div className="w-full h-40 rounded-md bg-gray-400"></div>
+            <div className="relative h-40">
+              <img
+                className="object-cover w-full h-full rounded-md"
+                src={
+                  "https://images.unsplash.com/photo-1634896941598-b6b500a502a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=912&q=80"
+                }
+              />
+              <div className="absolute p-4 flex justify-between w-full top-0 left-0">
+                <p className="text-white text-xs p-2 bg-gray-300 bg-opacity-20 rounded-full font-mono">
+                  Art
+                </p>
+              </div>
+              <div className="absolute w-full bottom-0 left-0 bg-black bg-opacity-40 rounded-b-md">
+                <p className="text-white text-center text-xs p-2 font-mono">
+                  Ends in : 50h : 30m :20s
+                </p>
+              </div>
+            </div>
             <div className="flex justify-between">
               <p className="text-white">Current Bid</p>
-              <p className="font-light text-md text-white">0.0005 ETH</p>
+              <p className="font-light text-md text-white font-mono">
+                0.0005 ETH
+              </p>
             </div>
-            <h1 className="text-white font-bold">Water bender</h1>
-          </div>
-        </div>
-        <div className="flex-shrink-0 p-3 w-72 h-80 rounded-lg bg-gray-300 bg-opacity-30 backdrop-filter blur-xl">
-          <div className="flex flex-col space-y-3">
-            <div className="w-full h-40 rounded-md bg-gray-400"></div>
-            <div className="flex justify-between">
-              <p className="text-white">Current Bid</p>
-              <p className="font-light text-md text-white">0.0005 ETH</p>
+            <h1 className="text-white font-bold font-mono">Water bender</h1>
+            <div className="flex -space-x-4">
+              <Img border={2} />
+              <Img border={2} />
+              <Img border={2} />
+              <Img border={2} />
+              <Img border={2} />
+              <div className="w-10 h-10 bg-blue-800 text-sm font-light font-mono text-white flex flex-col justify-center items-center rounded-full">
+                100+
+              </div>
             </div>
-            <h1 className="text-white font-bold">Water bender</h1>
-          </div>
-        </div>
-        <div className="flex-shrink-0 p-3 w-72 h-80 rounded-lg bg-gray-300 bg-opacity-30 backdrop-filter blur-xl">
-          <div className="flex flex-col space-y-3">
-            <div className="w-full h-40 rounded-md bg-gray-400"></div>
-            <div className="flex justify-between">
-              <p className="text-white">Current Bid</p>
-              <p className="font-light text-md text-white">0.0005 ETH</p>
-            </div>
-            <h1 className="text-white font-bold">Water bender</h1>
-          </div>
-        </div>
-        <div className="flex-shrink-0 p-3 w-72 h-80 rounded-lg bg-gray-300 bg-opacity-30 backdrop-filter blur-xl">
-          <div className="flex flex-col space-y-3">
-            <div className="w-full h-40 rounded-md bg-gray-400"></div>
-            <div className="flex justify-between">
-              <p className="text-white">Current Bid</p>
-              <p className="font-light text-md text-white">0.0005 ETH</p>
-            </div>
-            <h1 className="text-white font-bold">Water bender</h1>
-          </div>
-        </div>
-        <div className="flex-shrink-0 p-3 w-72 h-80 rounded-lg bg-gray-300 bg-opacity-30 backdrop-filter blur-xl">
-          <div className="flex flex-col space-y-3">
-            <div className="w-full h-40 rounded-md bg-gray-400"></div>
-            <div className="flex justify-between">
-              <p className="text-white">Current Bid</p>
-              <p className="font-light text-md text-white">0.0005 ETH</p>
-            </div>
-            <h1 className="text-white font-bold">Water bender</h1>
           </div>
         </div>
       </div>
--- src/components/Img.js
+++ src/components/Img.js
@@ -1,7 +1,7 @@
-export default function Img() {
+export default function Img({ border }) {
   return (
     <img
-      className="w-10 h-10 rounded-full object-cover"
+      className={`w-10 h-10 rounded-full object-cover border-${border} border-light-blue-500`}
       src={
         "https://images.unsplash.com/photo-1634896941598-b6b500a502a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=912&q=80"
       }
